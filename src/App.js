60c1365765a160e59320e6715fd7cfd6a61c61f0
Andrea Carolina <anndiegonzalez@gmail.com>
2020-09-02 15:22:07 -0500

    Componente Navbar y PokemonServices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { getAllPokemon } from './Services/Pokemon';
+import { getAllPokemon, getPokemon } from './Services/Pokemon';
+import Card from './Components/Card';
 import './App.css';
 
 function App() {
@@ -12,20 +13,39 @@ function App() {
   useEffect (() => {
     async function fetchData() {
       let response = await getAllPokemon(initialUrl);
-      console.log(response);
       setNextUrl(response.next);
       setNextUrl(response.previus);
+      let pokemon = await loadingPokemon(response.results);
+      console.log(pokemon);
       setLoading(false);
     }
     fetchData();
   }, [])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    })
+  );
+
+    setPokemonData(_pokemonData);
+  };
   return(
     <div>
-      { loading ? <h1>Loading...</h1> : (
-        <h1>Data is fetched</h1>
-      ) }
+      { 
+        loading ? <h1>Loading...</h1> : (
+          <>
+            <div className="grid-container">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key= {i} pokemon={pokemon}/>
+              })}
+            </div>
+          </>
+        ) 
+      }
     </div>
   );        
-}
+} 
 
 export default App;
